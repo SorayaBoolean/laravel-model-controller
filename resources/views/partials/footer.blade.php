605ed1355e8a6a4dd98ca7f6f15c63a3c99a3bd0
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-10-02 17:08:52 +0200

    starting structure, added footer and headed blade.php , app.blade.php and homepage.blade,

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <ul>
+        <img src="{{asset('images/logo.png')}}" alt="logo">
+        <li>Privacy</li>
+        <li>Come raggiungerci</li>
+    </ul>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li>Home</li>
+        <li>Contatti</li>
+        <li>Chi Siamo</li>
+        <li>Lavora con noi</li>
+        <li>Faqs</li>
+    </ul>
+</nav>
